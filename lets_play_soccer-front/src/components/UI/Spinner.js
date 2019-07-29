d40added42061d90fd0e1ef2ba97e135c70ec922
Beka <kasienov2001@gmail.com>
2019-07-29 20:01:25 +0600

    #20 Front: сделал loader

---
+++ lets_play_soccer-front/src/components/UI/Spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div className="Spinner">
+            <div className="outer">
+                <div className="box">
+                    <div className="shadow"/>
+                    <div className="gravity">
+                        <div className="ball"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Spinner;
--- lets_play_soccer-front/src/containers/Matches.js
+++ lets_play_soccer-front/src/containers/Matches.js
@@ -1,13 +1,28 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from "react-redux";
+import Spinner from "../components/UI/Spinner";
+import divWithClassName from "react-bootstrap/es/utils/divWithClassName";
 
 
 class Matches extends Component {
+    state = {
+        loading: false
+    };
 
     render() {
+        let form;
+        if(this.state.loading){
+            form = <Spinner />
+        } else {
+            form = (
+                <div>
+                    Here will be Matches (Main Page)
+                </div>
+            );
+        }
         return (
             <div>
-                Here will be Matches (Main Page)
+                {form}
             </div>
         )
     }
