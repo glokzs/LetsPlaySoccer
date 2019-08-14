852f9cb663e1fc992158fd39ef189b54da6d0645
Rabiga Sarbassova <iamrabiga@gmail.com>
2019-08-14 19:59:48 +0600

    #36 Front: Сверстан матч без поля

--- lets_play_soccer-front/src/containers/CreateMatch.js
+++ lets_play_soccer-front/src/containers/CreateMatch.js
@@ -1,12 +1,15 @@
 import React, { Component, Fragment } from 'react';
-import {DatePicker, TimePicker} from 'antd';
+import {DatePicker, Input, InputNumber, TimePicker} from 'antd';
 import 'moment/locale/ru';
 
 class CreateMatch extends Component {
     state = {
         date: null,
         time: null,
-        duration: null
+        duration: null,
+        team: null,
+        format: null,
+        price: null
     };
 
     onDateChange = (date, dateString) => {
@@ -14,7 +17,13 @@ class CreateMatch extends Component {
         this.setState({date})
     };
     onTimeChange = time => this.setState({time});
+    onTeamChange = team => this.setState({team});
+    onFormatChange = format => this.setState({format});
+    onPriceChange = price => this.setState({price});
+
+
     onDurationChange = (duration, durationValue) => this.setState({duration, durationValue});
+
     getHours = hours => {
         switch (hours) {
             case '1,0':
@@ -72,9 +81,29 @@ class CreateMatch extends Component {
         }
     };
 
+    getFormat = people => {
+        switch (people) {
+            case 1:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                return 'человек';
+            case 2:
+            case 3:
+            case 4:
+                return 'человека';
+            default:
+                return '';
+        }
+    };
+
     render() {
         const timeWord = this.getHours(this.state.durationValue);
-        console.log(timeWord);
+        const people = this.getFormat(this.state.format);
         return (
             <Fragment>
                 <header className='toolbar__header'>
@@ -84,6 +113,7 @@ class CreateMatch extends Component {
                         <button className='icon--x toolbar__header__btn--close'/>
                     </div>
                 </header>
+
                 <div className='row mt-4'>
                     <div className="col">
                         <div className='match__label'>Дата проведения</div>
@@ -96,7 +126,8 @@ class CreateMatch extends Component {
                         />
                     </div>
                 </div>
-                <div className='row mt-5'>
+
+                <div className='row mt-4'>
                     <div className="col-6">
                         <div className="match__label">Начало игры</div>
                         <TimePicker
@@ -124,6 +155,81 @@ class CreateMatch extends Component {
                                 disabledMinutes={() => [10,15,20,25,30,35,40,45,50,55]}
                             />
                         </div>
+                    </div>
+                </div>
+
+                <div className="row mt-4">
+                    <div className="col">
+                        <div className='match__label'>Выберите площадку для игры</div>
+                        <div className='position-relative'>
+                            <div className='icon--map-b match__input__icon'/>
+                            <Input
+                                placeholder='Выберите поле'
+                                className='match__input'
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='row mt-4'>
+                    <div className="col-6">
+                        <div className="match__label">В одной команде</div>
+                        <div className='position-relative'>
+                            <span className='match__people'>
+                                {people}
+                            </span>
+                            <InputNumber
+                                min={1}
+                                max={11}
+                                onChange={this.onFormatChange}
+                                value={this.state.format}
+                                className='match__input--number'
+                                placeholder='Количество игроков'
+                            />
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="match__label">Количество команд</div>
+                        <div className='position-relative'>
+                            <span className='timepicker__word'>
+                                {timeWord}
+                            </span>
+                            <InputNumber
+                                min={2}
+                                max={5}
+                                placeholder='Количество команд'
+                                onChange={this.onTeamChange}
+                                value={this.state.team}
+                                className='match__input--number'
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='row mt-4'>
+                    <div className="col-6">
+                        <div className="match__label">Стоимость площадки</div>
+                        <div className='position-relative'>
+                            <span className='match__input--price__icon'>₸</span>
+                            <InputNumber
+                                value={this.state.price}
+                                placeholder='Введите цену'
+                                formatter={value => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
+                                parser={value => value.replace(/\$\s?|( *)/g, '')}
+                                onChange={this.onPriceChange}
+                                className='match__input--number match__input--price'
+                                min={0}
+                            />
+                        </div>
+                    </div>
+                    <div className="col-6 d-flex flex-column justify-content-end">
+                        <div className='match__price'>{1200} ₸</div>
+                        <div className='match__label text-right'>С каждого игрока по</div>
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col">
+                        <button className='btn--primary'>Создать матч</button>
                     </div>
                 </div>
             </Fragment>
