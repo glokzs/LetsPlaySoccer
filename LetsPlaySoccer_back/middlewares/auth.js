0ca1c3560896923628b411c3c8ed02fbd530ccf4
Владислав Цой <tsoy.1996.vlad@gmail.com>
2019-09-03 18:02:40 +0600

    #77 Back: Создал middleware для авторизации пользователя

---
+++ LetsPlaySoccer_back/middlewares/auth.js
@@ -0,0 +1,17 @@
+const {User} = require("../sequelize");
+
+const auth = async (req, res, next) => {
+  const token = req.get('Authorization');
+  if (!token) {
+    return res.status(401).send({message: "Токен не предоставлен"});
+  }
+  const user = await User.findOne({token});
+  if(!user) {
+    return res.status(401).send({message: "Пользователь не найден"});
+  }
+
+  req.user = user;
+  next();
+};
+
+module.exports = auth;
